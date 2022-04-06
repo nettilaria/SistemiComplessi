9265f72ac7e13879f9421345599bfbf66feb1d24
nettilaria <ilaria.netti@studio.unibo.it>
2022-04-06 22:54:15 +0200

    Cambiamenti

--- Link.h
+++ Link.h
@@ -1,28 +1,33 @@
 #ifndef LINK_H
 #define LINK_H
-enum class LinkType : char //underling type int : 0 per null, 1 per small, etc
+enum class LinkType : char // underling type int : 0 per null, 1 per small, etc
 {
 
-    N, //null
-    SH, //small between houses
-    SS,  //small between house and sorting
-    M, //medium
-    B  //big
- 
-}; //Struttura gerarchica collegamenti
+    N,  // null
+    SH, // small between houses
+    SS, // small between house and sorting
+    M,  // medium
+    B   // big
+
+}; // Struttura gerarchica collegamenti
 class Link
 {
 private:
-    LinkType type_;
-    static const int basic_load = 3; //carico massimo del link più piccolo
+    LinkType type_ = LinkType::N;
+    static const int basic_load = 3; // carico massimo del link più piccolo
     int max_load_ = basic_load * static_cast<int>(type_);
-    int load_;
+    int load_ = 0;
+    int number_ = 0; // number that identifies the type of link during the print
 
 public:
-    Link(LinkType type, int max_load, int load);
+    Link(LinkType type, int load);
+    Link() = default;
     LinkType GetType() const;
+    void SetType(LinkType T);
     int GetMaxLoad() const;
     int GetLoad() const;
     void SetLoad(int newload);
+    int GetNumber() const;
+    void SetNumber(int n);
 };
 #endif
