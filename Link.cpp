28dcaef8db7b4ae4ebf65bb5b1faa63cee89eba0
nettilaria <ilaria.netti@studio.unibo.it>
2022-04-06 22:54:47 +0200

    Cambiamenti

--- Link.cpp
+++ Link.cpp
@@ -1,21 +1,34 @@
 #include "Link.h"
- Link::Link(LinkType type, int max_load, int load) : type_{type}, max_load_{max_load}, load_{load} {}
+Link::Link(LinkType type, int load) : type_{type}, load_{load} {}
 
-    LinkType Link::GetType() const
-    {
-        return type_;
-    };
-    int Link::GetMaxLoad() const
-    {
-        return max_load_;
-    }; 
-    int Link::GetLoad() const
-    {
+LinkType Link::GetType() const
+{
+    return type_;
+};
+int Link::GetMaxLoad() const
+{
+    return max_load_;
+};
+int Link::GetLoad() const
+{
 
-        return load_;
-    };
+    return load_;
+};
 
-    void Link::SetLoad(int newload)
-    {
-        load_ = newload;
-    };
+void Link::SetLoad(int newload)
+{
+    load_ = newload;
+};
+
+int Link::GetNumber() const
+{
+    return number_;
+};
+void Link::SetNumber(int n)
+{
+    number_ = n;
+};
+void Link::SetType(LinkType T)
+{
+    type_ = T;
+};
